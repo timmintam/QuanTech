3902bdbf4b9286879d949a1805755e38f92904eb
mskrabulis <114658187+mskrabulis@users.noreply.github.com>
2022-10-14 09:45:34 +0200

    Update force.py
    Generalised the code to an arbitrary number of particles

--- Quantech_Verlet/force.py
+++ Quantech_Verlet/force.py
@@ -11,102 +11,118 @@ eV = 1.602e-19
 angst = 1e-10
 dR = 0.002*angst
 
+seed = 50
+
 Ha = 4.36e-18
 angst = 1.0e-10
+
+#number of shots to calculate covariance matrix
+m = 10
+
 dR = 0.02 #removed the angst, since coordinates are given in angst
 
 dx = np.array([dR,0,0])
 dy = np.array([0,dR,0])
 dz = np.array([0,0,dR])
 
-def calc_forces(coordinates):
+
+def get_psi_0(coordinates):
 
     #get qubit operator corresponding to H(R)
-    (qubit_op, num_part, num_orb, problem, converter) = get_qubit_op([coordinates[0],coordinates[1]])
-
-    #get qubit operators coresponding to H+ (hamiltonian for atomic distances R + idR, R + jdR, R + kdR )
-    (qubit_op_plus_0_x, _, _, problem_plus_0_x, converter_plus_0_x) = get_qubit_op([coordinates[0] + dx,coordinates[1]])
-    (qubit_op_plus_0_y, _, _, problem_plus_0_y, converter_plus_0_y) = get_qubit_op([coordinates[0] + dy,coordinates[1]])
-    (qubit_op_plus_0_z, _, _, problem_plus_0_z, converter_plus_0_z) = get_qubit_op([coordinates[0] + dz,coordinates[1]])
-    
-    #get qubit operators coresponding to H- (hamiltonian for atomic distances R - idR, R - jdR, R - kdR )
-    (qubit_op_minus_0_x, num_part_x, num_orb_x, problem_minus_0_x, converter_minus_0_x) = get_qubit_op([coordinates[0] - dx,coordinates[1]])
-    (qubit_op_minus_0_y, num_part_y, num_orb_y, problem_minus_0_y, converter_minus_0_y) = get_qubit_op([coordinates[0] - dy,coordinates[1]])
-    (qubit_op_minus_0_z, num_part_z, num_orb_z, problem_minus_0_z, converter_minus_0_z) = get_qubit_op([coordinates[0] - dz,coordinates[1]])
- 
+    (qubit_op, num_part, num_orb, problem, converter) = get_qubit_op(coordinates)
     #get the ground state of H(R)
     psi_0,_ = calc_ground_state(qubit_op,num_part, num_orb, problem, converter)
-    
-    #define desired observable (H_+ - H_-)/(2dR) = force  
-    Obs0_x = (qubit_op_plus_0_x-qubit_op_minus_0_x)
-    Obs0_y = (qubit_op_plus_0_y-qubit_op_minus_0_y)
-    Obs0_z = (qubit_op_plus_0_z-qubit_op_minus_0_z)
-       
-    #get the expectation value <psi_0|O|psi_0>¨
-    
-    backend = Aer.get_backend('qasm_simulator') 
-    q_instance = QuantumInstance(backend, shots=8024)
-
-    
     psi_0 = CircuitStateFn(psi_0)
-    '''
-    # Option 1: Calucate the force according to Equation 7
-    measurable_expression = StateFn(Obs0, is_measurement=True).compose(psi_0) 
-    expectation = PauliExpectation().convert(measurable_expression)  
-    sampler = CircuitSampler(q_instance).convert(expectation) 
-    f0 = sampler.eval().real
-    '''
-    
-    #Option 2: Calculate the force according to Equation 6
-    
-    f0_plus_x = calc_energy(qubit_op_plus_0_x,num_part_x,num_orb_x,problem_plus_0_x,converter_plus_0_x)
-    f0_minus_x = calc_energy(qubit_op_minus_0_x,num_part_x,num_orb_x,problem_minus_0_x,converter_minus_0_x)
 
-    f0_plus_y = calc_energy(qubit_op_plus_0_y,num_part_y,num_orb_y,problem_plus_0_y,converter_plus_0_y)
-    f0_minus_y = calc_energy(qubit_op_minus_0_y,num_part_y,num_orb_y,problem_minus_0_y,converter_minus_0_y)
+    return psi_0
 
-    f0_plus_z = calc_energy(qubit_op_plus_0_z,num_part_z,num_orb_z,problem_plus_0_z,converter_plus_0_z)
-    f0_minus_z = calc_energy(qubit_op_minus_0_z,num_part_z,num_orb_z,problem_minus_0_z,converter_minus_0_z)
+#function to calculate covariance matrix
 
-    f0_x = f0_plus_x - f0_minus_x
-    f0_y = f0_plus_y - f0_minus_y
-    f0_z = f0_plus_z - f0_minus_z
-    
-    
-    ### repeat same for other atom ###
-    (qubit_op_plus_1_x, _, _, _, _) = get_qubit_op([coordinates[0],coordinates[1]+dx])
-    (qubit_op_plus_1_y, _, _, _, _) = get_qubit_op([coordinates[0],coordinates[1]+dy])
-    (qubit_op_plus_1_z, _, _, _, _) = get_qubit_op([coordinates[0],coordinates[1]+dz])
-    
-    (qubit_op_minus_1_x, num_particles_x, num_spin_orbitals_x, problem_x, converter_x) = get_qubit_op([coordinates[0],coordinates[1]-dx])
-    (qubit_op_minus_1_y, num_particles_y, num_spin_orbitals_y, problem_y, converter_y) = get_qubit_op([coordinates[0],coordinates[1]-dy])
-    (qubit_op_minus_1_z, num_particles_z, num_spin_orbitals_z, problem_z, converter_z) = get_qubit_op([coordinates[0],coordinates[1]-dz])
+def calc_cov(coordinates):
+    #calculate psi_0 at distances R for hamiltonian H(R)
+    psi_0 = get_psi_0(coordinates)
 
-    #define desired observable (H_+ - H_-)/(2dR) = force  
-    Obs1_x = (qubit_op_plus_1_x-qubit_op_minus_1_x)
-    Obs1_y = (qubit_op_plus_1_y-qubit_op_minus_1_y)
-    Obs1_z = (qubit_op_plus_1_z-qubit_op_minus_1_z)
-    
-    backend = Aer.get_backend('qasm_simulator') 
-    q_instance = QuantumInstance(backend, shots=1)
-    
-    measurable_expression_x = StateFn(Obs1_x, is_measurement=True).compose(psi_0)
-    measurable_expression_y = StateFn(Obs1_y, is_measurement=True).compose(psi_0) 
-    measurable_expression_z = StateFn(Obs1_z, is_measurement=True).compose(psi_0)
-    
-    expectation_x = PauliExpectation().convert(measurable_expression_x)
-    expectation_y = PauliExpectation().convert(measurable_expression_y)  
-    expectation_z = PauliExpectation().convert(measurable_expression_z)
-    
-    sampler_x = CircuitSampler(q_instance).convert(expectation_x)
-    sampler_y = CircuitSampler(q_instance).convert(expectation_y) 
-    sampler_z = CircuitSampler(q_instance).convert(expectation_z) 
-    
-    f1_x = sampler_x.eval().real
-    f1_y = sampler_y.eval().real
-    f1_z = sampler_z.eval().real
+    #array to hold force components    
+    force_components = []
+    for i in range(0,m):
+        #calculate forces for psi_0    
+        forces_i = calc_forces(coordinates,psi_0)
+        #temp array
+        new_forces = []
+        #transform [[f_x0,f_y0,f_z0],[f_x1,f_y1,f_z1]] to [f_x0,f_y0,f_z0,f_x1,f_y1,f_z1] and append it to force_components
+        for j in range(len(forces_i)):
+            if not new_forces:
+                new_forces = forces_i[j]
+            else:
+                new_forces += forces_i[j]
+        force_components.append(new_forces)            
 
-    f0 = np.array([f0_x,f0_y,f0_z])*Ha*(1/(2*dR*angst))
-    f1 = np.array([f1_x,f1_y,f1_z])*Ha*(1/(2*dR*angst))
+    #take the transpose of force_components
+    force_components = np.array(force_components).T
+    #calculate the covariance matrix
+    covariance_matrix = np.cov(force_components)                
+
+
+
+    return covariance_matrix
+
+def calc_forces(coordinates, psi = None):
+
+    backend = Aer.get_backend('qasm_simulator')
+
+    psi_0 = psi
+
+    if (psi_0 == None):
+        psi_0 = get_psi_0(coordinates)
+
+    forces = []
+    #loop over particles
     
-    return f0,f1
+    for i in range(len(coordinates)):
+
+    
+        #get qubit operators coresponding to H+ (hamiltonian for atomic distances R + idR, R + jdR, R + kdR )
+        (qubit_op_plus_x, _, _, problem_plus_x, converter_plus_x) = get_qubit_op(coordinates[0:i] + [(coordinates[i] + dx)] + coordinates[i+1:])
+        (qubit_op_plus_y, _, _, problem_plus_y, converter_plus_y) = get_qubit_op(coordinates[0:i] + [(coordinates[i] + dy)] + coordinates[i+1:])
+        (qubit_op_plus_z, _, _, problem_plus_z, converter_plus_z) = get_qubit_op(coordinates[0:i] + [(coordinates[i] + dz)] + coordinates[i+1:])
+        
+        #get qubit operators coresponding to H- (hamiltonian for atomic distances R - idR, R - jdR, R - kdR )
+        (qubit_op_minus_x, num_part_x, num_orb_x, problem_minus_x, converter_minus_x) = get_qubit_op(coordinates[0:i] + [(coordinates[i] - dx)] + coordinates[i+1:])
+        (qubit_op_minus_y, num_part_y, num_orb_y, problem_minus_y, converter_minus_y) = get_qubit_op(coordinates[0:i] + [(coordinates[i] - dy)] + coordinates[i+1:])
+        (qubit_op_minus_z, num_part_z, num_orb_z, problem_minus_z, converter_minus_z) = get_qubit_op(coordinates[0:i] + [(coordinates[i] - dz)] + coordinates[i+1:])
+        #get observables
+        Obs_x = (qubit_op_plus_x-qubit_op_minus_x)
+        Obs_y = (qubit_op_plus_y-qubit_op_minus_y)
+        Obs_z = (qubit_op_plus_z-qubit_op_minus_z)
+
+    
+        #get nuclear repulsion energies
+        rep_eng_plus_x = problem_plus_x.grouped_property_transformed.get_property("ElectronicEnergy").nuclear_repulsion_energy
+        rep_eng_plus_y = problem_plus_y.grouped_property_transformed.get_property("ElectronicEnergy").nuclear_repulsion_energy
+        rep_eng_plus_z = problem_plus_z.grouped_property_transformed.get_property("ElectronicEnergy").nuclear_repulsion_energy
+
+        rep_eng_minus_x = problem_minus_x.grouped_property_transformed.get_property("ElectronicEnergy").nuclear_repulsion_energy
+        rep_eng_minus_y = problem_minus_y.grouped_property_transformed.get_property("ElectronicEnergy").nuclear_repulsion_energy
+        rep_eng_minus_z = problem_minus_z.grouped_property_transformed.get_property("ElectronicEnergy").nuclear_repulsion_energy
+         
+        q_instance = QuantumInstance(backend, shots=1024, seed_transpiler=seed, seed_simulator=seed)
+
+        measurable_expression_x = StateFn(Obs_x, is_measurement=True).compose(psi_0)
+        measurable_expression_y = StateFn(Obs_y, is_measurement=True).compose(psi_0)
+        measurable_expression_z = StateFn(Obs_z, is_measurement=True).compose(psi_0)
+
+        expectation_x = PauliExpectation().convert(measurable_expression_x)
+        expectation_y = PauliExpectation().convert(measurable_expression_y)
+        expectation_z = PauliExpectation().convert(measurable_expression_z)
+
+        sampler_x = CircuitSampler(q_instance).convert(expectation_x)
+        sampler_y = CircuitSampler(q_instance).convert(expectation_y) 
+        sampler_z = CircuitSampler(q_instance).convert(expectation_z)
+
+        f_x = sampler_x.eval().real + rep_eng_plus_x - rep_eng_minus_x
+        f_y = sampler_y.eval().real + rep_eng_plus_y - rep_eng_minus_y
+        f_z = sampler_z.eval().real + rep_eng_plus_z - rep_eng_minus_z
+
+        forces.append(np.array([f_x,f_y,f_z])*Ha*(1/(2*dR*angst)))
+    
+    return forces
